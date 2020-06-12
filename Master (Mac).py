14252cb31e78279284a4f58e04c4d51c775fd8c4
Kipland Melton <kipmelton@yahoo.com>
2020-06-11 21:43:25 -0500

    Complete reupload

--- Master (Mac).py
+++ Master (Mac).py
@@ -2,55 +2,73 @@
 Kipland Melton
 A program to notify you of your bad posture
 
-11/30/2019
+Mac Build .35M
+
+Whats new:
+
+    time entry and save, implemented in full effect mac version
+
+PZ Software
+May 23, 2020
 """
+
 # Graphics based UI library
 from tkinter import *
 # Image manipulation with pillow
-from PIL import Image
+from PIL import Image, ImageTk
 # Time library for alarm function
 from time import sleep
 
-window_X = 240
-window_Y = 50
-
+window_X = 270
+window_Y = 80
 
 """
-wTaskbar_posX = 1670
-wTaskbar_posY = 960
+Home page can be referred to as page one, options page two, etc..
+Could be smarter to create an algorithm for keep up with page structure on the fly instead
+of hard coding in back buttons on every page.. just a thought
+
+
+
+Simply add a counter that is increased/decreased based on which pages are chosen?
+
+Start : Page 1
+> Clicks options
+>> Page 2
+>>> Clicks back, page = 1
+>>>> Clicks forward, page = 2 
 """
 
 
 class Main_Window:
 
     def __init__(self, master):
+
+        self.page = 1
+        
         self.master = master
         self.frame = Frame(self.master)
 
         self.master.title("PostureAlarm")
 
-        self.b1 = Button(self.master, text="Close", width=4, command=master.quit)
-        self.b2 = Button(self.master, text="+", width=4,command=self.new_window)
+        self.b1 = Button(self.master, text="Exit", width=3, fg='black',command=master.quit)
+        self.b2 = Button(self.master, text="+", width=3, bg='#433e46',command=self.options_navigation)
 
-        self.info_text = Label(self.master, text='Sit Up!')
+        self.info_text = Label(self.master, text='Sit Up Buddy!')
         self.info_text.pack()
 
-        self.b1.place(x=60,y=20)
-        self.b2.place(x=120,y=20)
-
-        self.back_button = Button(self.master, image=backbutton,  height=16, width = 16) # Back Button
-        self.forward_button = Button(self.master, image=forwardbutton, height=16, width = 16) # Forward Button
-        self.back_button.place(x=0,y=0)
-        self.forward_button.place(x=40,y=0)
+        self.b1.place(x=70,y=20)
+        self.b2.place(x=130,y=20)
 
         self.frame.pack()
 
-    def new_window(self):
+    def options_navigation(self):
         self.master.withdraw()
         self.newWindow = Toplevel(self.master)
         Opt_activate = Options_Window(self.newWindow)
         self.newWindow.minsize(window_X,window_Y)
         self.newWindow.geometry("+1670+960")
+        self.page += 1
+        print(self.page)
 
 
 
@@ -65,37 +83,68 @@ class Options_Window():     # Classifies the alternate options menu
         self.options_text = Label(self.master, text="Set Alert Interval (Minutes)") # Window Description
         self.options_text.pack()
 
-        Minute = 60 # Minute is to be multiplied by whichever number the user enters
-        User_Time = StringVar()
-
-        self.back_button = Button(self.master, image=backbutton) # Back Button
-        self.forward_button = Button(self.master, image=forwardbutton) # Forward Button
+        self.back_button = Button(self.master, image=back_image_resize, command=self.back_button) # Back Button
+        self.forward_button = Button(self.master, image=forward_image_resize) # Forward Button
         self.back_button.place(x=0,y=0)
-        self.forward_button.place(x=17,y=0)
+        self.forward_button.place(x=20,y=0)
 
         self.frame.pack()
 
-        self.master.wm_iconbitmap('officechair.ico') # Declares icon for options window
+        self.master.wm_iconbitmap('officechair.icns') # Declares icon for options window
 
-        self.Alarm_entry = Entry(self.master, width=4, textvariable=User_Time).place(x=100, y=23)  # Creates text box for time entry
+        self.timeEntrySave = StringVar()
+        self.Alarm_entry = Entry(self.master, width=4, textvariable=self.timeEntrySave)
+        self.Alarm_entry.place(x=100, y=23)  # Creates text box for time entry
+        self.submit_time = Button(self.master, text='Submit',command=self.TimeEntry_ToFile)
+        self.submit_time.place(x=85,y=50)
 
+    def TimeEntry_ToFile(self):
+        usertime = self.Alarm_entry.get()
+        with open('time_entry.txt', 'a') as file_object:
+            file_object.write(usertime) 
+    
+
+    def back_button(self):
+
+        self.master.withdraw()
+        self.newWindow = Toplevel(self.master)
+        Opt_activate = Main_Window(self.newWindow)
+        self.newWindow.minsize(window_X,window_Y)
+        self.newWindow.geometry("+1670+960")
+        self.page -= 1
+
+        
 
 
 if __name__ == '__main__':
     root = Tk()
 
-    # Couldn't recognize the data in image file?
-    # > For some reason tkinter doesnt want to accept any file type besides .gif , not jpg or png.
-    backbutton = PhotoImage(file=r'/Users/kipland/Documents/Python/PostureAlarm/Images/arrow-left.gif')
-    forwardbutton = PhotoImage(file=r'/Users/kipland/Documents/Python/PostureAlarm/Images/arrow-right.gif')
+    # File path for back/forward buttons
+    backbutton = r"/Users/kipland/Documents/Python/PostureAlarm/Images/arrow-left.png"
+    forwardbutton = r"/Users/kipland/Documents/Python/PostureAlarm/Images/arrow-right.png"
+
+    # Image packing and resizing
+    back_image = Image.open(backbutton)
+    forward_image = Image.open(forwardbutton)
+    back_image = back_image.resize((12,12), Image.ANTIALIAS)
+    forward_image = forward_image.resize((12,12), Image.ANTIALIAS)
+
+    # Final images
+    back_image_resize = ImageTk.PhotoImage(back_image)
+    forward_image_resize = ImageTk.PhotoImage(forward_image)
 
 
+
+
+    ###################
+    # Root properties #
+    ###################
     MainWindow = Main_Window(root)
     root.minsize(window_X,window_Y)
 
 
     root.geometry("+1670+960")  #+1670+1000 for noti window
-    root.wm_iconbitmap('officechair.ico')
+    root.wm_iconbitmap('officechair.icns')
     root.mainloop()
 
 
